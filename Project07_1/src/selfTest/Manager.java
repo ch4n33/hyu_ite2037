909654bce33585c872906391eee914d692e1f4c0
임종찬 <nowwhd987@hanyang.ac.kr>
2020-04-30 17:22:16 +0900

    Project07_1

---
+++ Project07_1/src/selfTest/Manager.java
@@ -0,0 +1,19 @@
+package selfTest;
+
+public class Manager extends Employee{
+
+	private int officeNum;
+	private String team;
+	
+	public Manager(String name, int employeeNum, int officeNum, String team) {
+		super(name, employeeNum);
+		this.officeNum = officeNum;
+		this.team = team;
+	}
+	
+	public String toString() {
+		return "Name :" + this.getName() +
+				"\nlocation : " + this.getDepartment() +
+				", " + this.officeNum;
+	}
+}
