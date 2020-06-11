9f2af4e156a618a02f71e562d21c98c11c145797
임종찬 <nowwhd987@hanyang.ac.kr>
2020-06-11 23:36:11 +0900

    Project13_1

---
+++ Project13_1/src/selfTest/ChocolateBoiler.java
@@ -0,0 +1,54 @@
+package selfTest;
+
+public class ChocolateBoiler {
+	private boolean empty;
+	private boolean boiled;
+	
+    // Change and add the code
+	private volatile static ChocolateBoiler instance = new ChocolateBoiler();
+	
+	public static ChocolateBoiler getInstance() {
+		return instance;
+	}
+    
+	private ChocolateBoiler() {
+		empty = true;
+		boiled = false;
+	}
+	
+	public void fill() {
+		if(isEmpty()) {
+			empty = false;
+			boiled = false;
+			System.out.println("Filling with mixture");
+		}
+		else
+			System.out.println("Already filled");
+	}
+	
+	public void drain() {
+		if(!isEmpty() && isBoiled()) {
+			empty = true;
+			System.out.println("Draining the mixture");
+		}
+		else
+			System.out.println("Already drained");
+	}
+	
+	public void boil() {
+		if(!isEmpty() && !isBoiled()) {
+			boiled = true;
+			System.out.println("Boiling the mixture");
+		}
+		else
+			System.out.println("Already boiled");
+	}
+	
+	public boolean isEmpty() {
+		return empty;
+	}
+	
+	public boolean isBoiled() {
+		return boiled;
+	}
+}
